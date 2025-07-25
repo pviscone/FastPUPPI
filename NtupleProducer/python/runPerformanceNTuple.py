b837bbff0075a02ed263a612d8112a7c8740392a
Piero Viscone <pieroviscone@gmail.com>
2025-07-25 18:33:01 +0200

    Added hwCaloEta to user floats in tkele table

--- NtupleProducer/python/runPerformanceNTuple.py
+++ NtupleProducer/python/runPerformanceNTuple.py
@@ -651,6 +651,7 @@ def addTkEG(doL1=False, doL2=True, postfix=""):
         tkEleTable.variables.scaled_caloTkAbsDeta = LazyVar('userFloat("scaled_caloTkAbsDeta")', float, precision=8)
         tkEleTable.variables.scaled_caloTkAbsDphi = LazyVar('userFloat("scaled_caloTkAbsDphi")', float, precision=8)
 
+        tkEleTable.variables.hwCaloEta = LazyVar('userFloat("hwCaloEta")', float, precision=8)
 
         return tkEmTable, tkEleTable
                                    
