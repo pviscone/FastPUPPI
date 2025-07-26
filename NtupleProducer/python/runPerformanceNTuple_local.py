98b57c38f113b0ef8743108745810a7650afb5e3
Piero Viscone <pieroviscone@gmail.com>
2025-07-26 22:44:12 +0200

    Added ptCorr

--- NtupleProducer/python/runPerformanceNTuple_local.py
+++ NtupleProducer/python/runPerformanceNTuple_local.py
@@ -4,12 +4,12 @@ process.source = process.source.clone(
     fileNames = cms.untracked.vstring('root://eoscms.cern.ch//eos/cms/store/cmst3/group/l1tr/pviscone/SampleFactory/DoubleElectron_FlatPt_1To100__chain_Phase2Spring24GS-INFP_PU200/SampleFactory/250714_121635/inputs140X_103.root'),
 )
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(20))
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(200))
 
 noResp()
 addGenLep([11,22])
 addTkEG()
-addHGCalTPs()
-addStaEG()
-addDecodedTk()
-addDecodedCalo()
+#addHGCalTPs()
+#addStaEG()
+#addDecodedTk()
+#addDecodedCalo()
